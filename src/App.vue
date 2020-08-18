25ded69c075773f23ef2ab3955e8f4c89e5bee5c
youpoko0404 <clan.sf123@gmail.com>
2020-08-18 20:28:37 +0700

    button switch

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="เลือกเลยนะครับ" />
+    <hr />
+    <div class="row">
+      <div class="col-6">
+        <img alt="Vue logo" src="./assets/1.jpg" />
+        <h1>{{total1}}</h1>
+      </div>
+      <div class="col-6">
+        <img alt="Vue logo" src="./assets/2.jpg" />
+        <h1>{{total2}}</h1>
+      </div>
+      <div class="col-6">
+        <CSButton @cc="sum" />
+      </div>
+      <div class="col-6">
+        <CSButton @cc="sum1" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import CSButton from "./components/CsButton";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CSButton,
+  },
+
+  data: function () {
+    return {
+      total1: 0,
+      total2: 0,
+    };
+  },
+
+  methods: {
+    sum(counterValue) {
+      this.total1 = counterValue;
+    },
+    sum1(counterValue) {
+      this.total2 = counterValue;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/CsButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button v-on:click="upFunction" class="btn btn-primary">LIKE {{lable}}</button>
+    <button v-on:click="downFunction" class="btn btn-success ml-3">LOVE {{lable}}</button>
+    <button v-on:click="LoveFunction" class="btn btn-danger ml-3">DISLIKE {{lable}}</button>
+    <button v-on:click="ResetFunction" class="btn btn-warning ml-3">RESET {{lable}}</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    upFunction: function () {
+      this.counter += 1;
+      this.$emit("cc", this.counter);
+    },
+
+    downFunction: function () {
+      this.counter += 5;
+      this.$emit("cc", this.counter);
+    },
+
+    LoveFunction: function () {
+      this.counter -= 10;
+      this.$emit("cc", this.counter);
+    },
+
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+<style>
+</style>
